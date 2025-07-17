25d14ff10af5029ddd46653be251b3904b4e07f6
laila2099 <laila.sankar@gmail.com>
2025-07-17 19:24:39 +0300

    Add files via upload

---
+++ lib/screens/new_subscription.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:trackizer/controllers/subscription_controller.dart';
+import 'package:trackizer/models/subscription_model.dart';
+import 'package:trackizer/views/subscription_view.dart';
+
+class NewSubscription extends StatefulWidget {
+  const NewSubscription({super.key});
+
+  @override
+  State<NewSubscription> createState() => _NewSubscriptionState();
+}
+
+class _NewSubscriptionState extends State<NewSubscription> {
+  late final NewSubscriptionController _controller;
+  late final SubscriptionModel _model;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = NewSubscriptionController();
+    _model = SubscriptionModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NewSubscriptionView(controller: _controller, model: _model);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
