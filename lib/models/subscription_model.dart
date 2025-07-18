59e4a2d5fa5b7075c0a95c416865d1b2172b99ae
laila2099 <laila.sankar@gmail.com>
2025-07-19 01:05:18 +0300

    Delete lib directory

--- lib/models/subscription_model.dart
+++
@@ -1,9 +0,0 @@
-class SubscriptionModel {
-  final List<String> imgList = [
-    'assets/images/OneDrive Logo-2.png',
-    'assets/images/HBO GO Logo.png',
-    'assets/images/Spotify Logo-2.png',
-  ];
-
-  final List<String> nameList = ['OneDrive', 'HBO GO', 'Spotify'];
-}
