26aba60e9059aa6cf74dcdbc3e82f9c0883bc7e4
Zaid43594 <zaidfocalx@gmail.com>
2025-07-19 12:37:12 -0700

    Add files via upload

---
+++ lib/modle/subscription_model.dart
@@ -0,0 +1,12 @@
+import 'package:trackizer/core/constants/image_paths.dart';
+
+class SubscriptionModel {
+  final List<String> imgList = [
+    ImagePaths.onedrive,
+    ImagePaths.hbogo,
+    ImagePaths.spotify,
+  
+  ];
+
+  final List<String> nameList = ['OneDrive', 'HBO GO', 'Spotify'];
+}
