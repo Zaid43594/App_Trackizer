b0f419e08e0d58f682aa8482483d7e680e9c1585
Zaid43594 <zaidfocalx@gmail.com>
2025-07-19 12:48:05 -0700

    Add files via upload

---
+++ lib/core/constants/image_paths.dart
@@ -0,0 +1,26 @@
+class ImagePaths {
+  static const String spotify = 'assets/images/Spotify_Logo.png';
+  static const String onedrive = 'assets/images/OneDrive_Logo.png';
+  static const String youtube = 'assets/images/YT_Premium_Lgoo.png';
+  static const String netflix = 'assets/images/Netflix_Logo.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String cricles = 'assets/images/Main_Circle.png';
+  static const String date = 'assets/images/Date.png';
+  static const String calendar = 'assets/images/Calendar.png';
+  static const String budgets = 'assets/images/Budgets.png';
+  static const String wallet = 'assets/images/wallet.png';
+  static const String home = 'assets/images/Home.png';
+  static const String creditcard = 'assets/images/Credit Cards.png';
+  static const String bg1 = 'assets/images/log-bg1r.png';
+  static const String bg2 = 'assets/images/log-bg2l.png';
+  static const String Frame = 'assets/images/Frame.png';
+  static const String appicon = 'assets/images/App_icon.png';
+  static const String chart = 'assets/images/Chart.png';
+  static const String divider = 'assets/images/Divider.png';
+  static const String ellipse = 'assets/images/Ellipse.png';
+  static const String faceid = 'assets/images/FaceID.png';
+  static const String money = 'assets/images/Money.png';
+  static const String sorting = 'assets/images/Sorting.png';
+  static const String image = 'assets/images/Image.png';
+  static const String hbogo = 'assets/images/HBO_GO_Logo.png';
+}
