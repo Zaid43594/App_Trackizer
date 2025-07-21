720da6b15b0b4da9b0902b70de31cff35e966855
laila2099 <laila.sankar@gmail.com>
2025-07-21 03:18:58 +0300

    Add files via upload

---
+++ lib/views/customs/custom_bar_icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyIcons {
+  static const _kFontFam = 'IcoMoon';
+
+  static const IconData home = IconData(0xe904, fontFamily: _kFontFam);
+  static const IconData grid = IconData(0xe901, fontFamily: _kFontFam);
+  static const IconData cal = IconData(0xe902, fontFamily: _kFontFam);
+  static const IconData wal = IconData(0xe903, fontFamily: _kFontFam);
+}
